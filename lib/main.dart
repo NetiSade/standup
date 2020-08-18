8ef77cd6fd0358acbee9a0e253a27d1d400ae381
Neti Sade <netanel@afimilk.co.il>
2020-08-18 22:41:54 +0300

    Added Break time support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -40,20 +41,180 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final _defaultDurationInMin = 45;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  int _workDurationInMin = 45;
+  int _breakDurationInMin = 5;
   Timer _timer;
   bool _running = false;
   DateTime _nextAlert;
   Duration _timeUntilNextAlert;
+  bool _autoRestart = false;
+  bool _inBreak = false;
 
   @override
   void initState() {
     _getAlertTimeFromLocal();
+    _getPrefsFromLocal();
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _timer.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StandUP!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _getTimeText(),
+              style: Theme.of(context)
+                  .textTheme
+                  .headline1
+                  .copyWith(color: Theme.of(context).primaryColor),
+            ),
+            if (_running && _autoRestart)
+              Text(
+                _inBreak ? '⛱️ Break Time ⛱️' : '🛠️ Work Time 🛠️',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    .copyWith(color: Theme.of(context).accentColor),
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (!_running) {
+              _startTimer();
+            } else {
+              _stopTimer();
+            }
+          },
+          child: AnimatedCrossFade(
+            firstChild: Icon(Icons.play_arrow),
+            secondChild: Icon(Icons.stop),
+            crossFadeState:
+                _running ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+            duration: Duration(milliseconds: 300),
+          )),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                child: Text('Settings',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Theme.of(context).accentColor))),
+            SwitchListTile(
+              title: Text("Auto Restart"),
+              onChanged: (bool value) {
+                setState(() {
+                  _autoRestart = value;
+                  _savePrefsToLocal();
+                });
+              },
+              value: _autoRestart,
+            ),
+            ListTile(
+              title: Text('Work Time:'),
+              trailing: Container(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(
+                          Icons.remove,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          if (_workDurationInMin > 20)
+                            setState(() {
+                              _workDurationInMin -= 5;
+                              _savePrefsToLocal();
+                            });
+                        }),
+                    Text(_workDurationInMin.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1
+                            .copyWith(color: Theme.of(context).accentColor)),
+                    IconButton(
+                        icon: Icon(
+                          Icons.add,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          if (_workDurationInMin < 60)
+                            setState(() {
+                              _workDurationInMin += 5;
+                              _savePrefsToLocal();
+                            });
+                        }),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Break Time:'),
+              trailing: Container(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(
+                          Icons.remove,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          if (_breakDurationInMin > 1)
+                            setState(() {
+                              _breakDurationInMin -= 1;
+                            });
+                        }),
+                    Text(_breakDurationInMin.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1
+                            .copyWith(color: Theme.of(context).accentColor)),
+                    IconButton(
+                        icon: Icon(
+                          Icons.add,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          if (_breakDurationInMin < 15)
+                            setState(() {
+                              _breakDurationInMin += 1;
+                            });
+                        }),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   void _startTimer() {
+    _nextAlert = DateTime.now().add(
+        Duration(minutes: _inBreak ? _breakDurationInMin : _workDurationInMin));
     if (_nextAlert == null || _nextAlert.isBefore(DateTime.now())) {
       return;
     }
@@ -71,6 +232,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     _schedulNotification();
     _saveAlertTimeToLocal();
+    _savePrefsToLocal();
 
     setState(() {
       _running = true;
@@ -79,10 +241,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _onTimerTick(Timer timer) {
     if (_nextAlert == null || DateTime.now().isAfter(_nextAlert)) {
-      timer.cancel();
-      setState(() {
-        _running = false;
-      });
+      if (_autoRestart) {
+        setState(() {
+          _inBreak = !_inBreak;
+        });
+        _startTimer();
+      } else {
+        timer.cancel();
+        setState(() {
+          _running = false;
+        });
+      }
     } else {
       _updateTimeUntilNextAlert();
     }
@@ -103,6 +272,14 @@ class _MyHomePageState extends State<MyHomePage> {
     await prefs.setInt('nextAlert', _nextAlert.millisecondsSinceEpoch);
   }
 
+  _savePrefsToLocal() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('workDurationInMin', _workDurationInMin);
+    await prefs.setInt('breakDurationInMin', _breakDurationInMin);
+    await prefs.setBool('autoRestart', _autoRestart);
+    await prefs.setBool('inBreak', _inBreak);
+  }
+
   _clearNextAlertFromLocal() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('nextAlert', 0);
@@ -121,7 +298,26 @@ class _MyHomePageState extends State<MyHomePage> {
         _startTimer();
       } else {
         _clearNextAlertFromLocal();
+        setState(() {
+          _inBreak = false;
+        });
       }
+    }
+  }
+
+  _getPrefsFromLocal() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey('workDurationInMin')) {
+      _workDurationInMin = prefs.getInt('workDurationInMin');
+    }
+    if (prefs.containsKey('breakDurationInMin')) {
+      _breakDurationInMin = prefs.getInt('breakDurationInMin');
+    }
+    if (prefs.containsKey('autoRestart')) {
+      _autoRestart = prefs.getBool('autoRestart');
+    }
+    if (prefs.containsKey('inBreak')) {
+      _inBreak = prefs.getBool('inBreak');
     }
   }
 
@@ -130,57 +326,15 @@ class _MyHomePageState extends State<MyHomePage> {
     await flutterLocalNotificationsPlugin.cancelAll();
     setState(() {
       _running = false;
+      _inBreak = false;
     });
+
     _clearNextAlertFromLocal();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StandUP!'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _getTimeText(),
-              style: Theme.of(context)
-                  .textTheme
-                  .headline1
-                  .copyWith(color: Theme.of(context).primaryColor),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: !_running
-          ? FloatingActionButton(
-              onPressed: () {
-                _nextAlert = DateTime.now()
-                    .add(Duration(minutes: _defaultDurationInMin));
-                _startTimer();
-              },
-              tooltip: 'Start Timer',
-              child: Icon(Icons.timer),
-            )
-          : FloatingActionButton(
-              onPressed: _stopTimer,
-              tooltip: 'Stop Timer',
-              child: Icon(Icons.stop),
-            ),
-    );
   }
 
   String _getTimeText() {
     if (!_running) {
-      return '$_defaultDurationInMin:00';
+      return '$_workDurationInMin:00';
     }
     return _printDuration(_timeUntilNextAlert);
   }
@@ -194,22 +348,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _schedulNotification() async {
     await flutterLocalNotificationsPlugin.cancelAll();
-    var scheduledNotificationDateTime = _nextAlert;
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    final scheduledNotificationDateTime = _nextAlert;
+    final androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'StandUp', 'StandUpReminder', 'StandUp take 5 reminder',
         importance: Importance.Max,
         priority: Priority.Max,
         enableVibration: true,
         enableLights: true,
         channelShowBadge: true,
-        playSound: true);
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    NotificationDetails platformChannelSpecifics = NotificationDetails(
+        playSound: true,
+        autoCancel: true,
+        showWhen: true);
+    final iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    final platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
     await flutterLocalNotificationsPlugin.schedule(
       0,
       'StundUp!',
-      'StundUp and take 5!',
+      _inBreak ? 'Time to back to work!' : 'StundUp and take a break!',
       scheduledNotificationDateTime,
       platformChannelSpecifics,
       androidAllowWhileIdle: true,
